ca3a1e14b867e35f7d6946305ca1cb2e26ae1dab
tanmaychimurkar <tanmay.chimurkar@uzh.ch>
2022-09-13 00:30:52 +0200

    added solution for fibonacci sequence

---
+++ leetcode/509_fibonacci_sequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fib(self, n):
+
+        mem = {}
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        # use caching to avoid recomputing fibonacci sequence everytime
+
+        if n not in mem:
+            mem[n] = self.fib(n - 1) + self.fib(n - 2)
+        return mem[n]
+
+
+_obj = Solution()
+_obj.fib(6)
